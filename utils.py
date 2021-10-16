d39e90bc18d52910dd8540f4334e91586b6d4dac
Mortis66666 <87415361+Mortis66666@users.noreply.github.com>
2021-10-16 11:33:37 +0800

    Added mysql database to record the highest score

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 import pygame
 import random
+import mysql.connector
+from constants import *
+
+db = mysql.connector.connect(**kwargs)
+
+mycursor = db.cursor()
 
 
 def enlarge(object,factor):
@@ -12,3 +18,32 @@ def is_cacnea(pos):
 def is_jump(garchomp):
 
     return garchomp.y != 197
+
+
+
+def break_record(score) -> bool:
+    mycursor.execute("SELECT record FROM Record")
+
+    record = 0
+
+    for x in mycursor:
+        record += x[0]
+
+    if record > score:
+        return False
+
+    mycursor.execute("UPDATE Record SET record = %s WHERE record = %s",(score,record))
+    db.commit()
+
+    return True
+
+def get_record() -> int:
+
+    mycursor.execute("SELECT record FROM Record")
+
+    rec = 0
+
+    for x in mycursor:
+        rec = x[0]
+
+    return rec
