fb071ebc5d757efc7f5f9b10011a65eb3d81c7b1
Mortis66666 <87415361+Mortis66666@users.noreply.github.com>
2021-10-16 21:14:50 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 kwargs = {
-    "host": "localhost", #host
-    "user": "mortis666", #user
-    "passwd": "653161", #password
-    "database": "garchompjump" #database
+    "host": "", #host
+    "user": "", #user
+    "passwd": "", #password
+    "database": "" #database
 }
